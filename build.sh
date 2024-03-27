61c4caeaf61ad38f239be35156845d33290520be
hkchenhao <hkchenhao@qq.com>
2024-03-27 18:12:07 +0800

    update

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ cd -
 cd ../uboot
 make distclean
 make CROSS_COMPILE=aarch64-none-linux-gnu- rk3399_defconfig
-# make CROSS_COMPILE=aarch64-none-linux-gnu- menuconfig && make savedefconfig && cp defconfig ./configs/rk3399_defconfig
+# make CROSS_COMPILE=aarch64-none-linux-gnu- menuconfig && make savedefconfig && mv defconfig ./configs/rk3399_defconfig
 make CROSS_COMPILE=aarch64-none-linux-gnu- -j32
 ./../loader/tools/loaderimage --pack --uboot u-boot.bin uboot.img 0x200000 --size 2048 2
 mv uboot.img ../build/image
